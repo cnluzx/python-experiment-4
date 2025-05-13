e9f4072fbe423ca3f0237fb13d7046568dbfce0a
cnluzx <cnluzx@outlook.com>
2025-05-13 15:41:30 +0800

    first commit

---
+++ LSM.py
@@ -0,0 +1,8 @@
+###LSM (Least Squares Method)
+
+import numpy as np
+
+x_years=np.array([1994,1995,1996,1997,1998,1999,2000,2001,2002,2003])
+y_tones=np.array([67.052,68.008,69.083,72.024,73.400,72.063,74.669,74.487,74.065,76.777])
+
+def linear_model(x,y):
